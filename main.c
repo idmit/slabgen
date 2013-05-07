f97e4724db776862db3430d4aa3d48c2400180bb
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2013-05-07 21:54:48 +0400

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Graph
+//
+//  Created by ivan on 07.05.13.
+//  Copyright (c) 2013 dmitrievsky. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
